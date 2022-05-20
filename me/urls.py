51cab40e548f3b85ad4855df2f91c3925d65a810
Stas Ukolov <ukoloff@gmail.com>
2022-05-20 12:06:39 +0500

    Add Home page

--- me/urls.py
+++ me/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls'))
+    path('polls/', include('polls.urls')),
+    path('', TemplateView.as_view(template_name="home/index.html")),
+    path('accounts/', include('allauth.urls')),
+    path('logout', LogoutView.as_view()),
 ]
